f4c78cb94005cac199bbbfe2ec1ff62ae310e981
budemznakomyshop-crypto <budemznakomyshop@gmail.com>
2025-11-18 14:30:06 +0300

    Update server.js

--- server.js
+++ server.js
@@ -41,8 +41,6 @@ app.post("/order", async (req, res) => {
   const address = customer?.address ?? addressTop;
   const comment = customer?.comment ?? commentTop;
 
-  const cafe = selectedCafe ?? "-";
-
   // Формируем продукт: сначала проверяем product из тела, иначе соберём из items
   const product = (() => {
     if (productTop) return productTop;
@@ -82,7 +80,6 @@ app.post("/order", async (req, res) => {
 Телефон: ${safe(phone)}
 Email: ${safe(email)}
 Адрес: ${safe(address)}
-${deliveryType === "pickup" ? `cafe: ${safe(cafe)}` : ""}
 Комментарий: ${safe(comment)}
 Продукт: ${safe(product)}`;
 
