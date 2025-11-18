c0cd1d3a53eff8e7b9b1e41ac142935932f1d8ea
budemznakomyshop-crypto <budemznakomyshop@gmail.com>
2025-11-18 14:36:57 +0300

    Update server.js

--- server.js
+++ server.js
@@ -118,3 +118,7 @@ app.get("/", (req, res) => res.send("Server is working"));
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.get("/", (req, res) => res.send("Server is working"));
+
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
